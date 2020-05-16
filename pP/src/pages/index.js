f6b768231a8df4baf844debe5b13b2a1770241a4
Vladyslav <ganushkevychv@gmail.com>
2020-05-16 23:19:52 +0200

    graphql added to use gatsby-image plugin and footer added to index page

--- pP/src/pages/index.js
+++ pP/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { Link } from "gatsby"
 import {useTranslation} from "react-i18next";
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
 import "../translations/i18n.js"
 
-const IndexPage = () => {
+
+const IndexPage = (props) => {
   const {t} = useTranslation();
 
   return (
@@ -16,11 +18,29 @@ const IndexPage = () => {
   <p>{t("index.slogan")}</p>
     
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+     
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <div className="site-footer">
+      <h4 className="text-center">All rights reserved© {new Date().getFullYear()}, Created by
+            {` `}
+            <a href="https://vladyslav-ganushkevych.netlify.app/"><Img className="link-logo" fluid={props.data.me.childImageSharp.fluid} alt="logo"/></a></h4>
+      
+     
+      </div>
   </Layout>
   )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    me: file(relativePath: { eq: "me.png" }){
+      childImageSharp {
+        fluid(maxHeight: 50, maxWidth: 50){
+          ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}`;
